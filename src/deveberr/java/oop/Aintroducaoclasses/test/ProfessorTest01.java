5cfbd92fc8f97eb78b3d610297b29033e3c1c288
Eber J. <dev.eberr@gmail.com>
2024-05-06 19:28:58 -0300

    feat: adicionar arquivos de aula

--- src/deveberr/java/oop/Aintroducaoclasses/test/ProfessorTest01.java
+++ src/deveberr/java/oop/Aintroducaoclasses/test/ProfessorTest01.java
@@ -6,7 +6,7 @@ public class ProfessorTest01 {
     public static void main(String[] args) {
         Professor professor = new Professor();
         professor.nome = "Mestre Kame";
-        professor.idade = 300;
+        professor.idade = 140;
         professor.sexo = 'M';
         System.out.println("Professor: " + professor.nome + " " + "Idade: " + professor.idade + " " + "Sexo:" + " " + professor.sexo);
     }
